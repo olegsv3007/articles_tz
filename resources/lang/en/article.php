af0c0f92569087c63687023ccdb82fcc8cb1ad95
Oleg Sviridov <olegsv3007@yandex.ru>
2022-03-11 20:38:24 +0200

    add localization

--- resources/lang/en/article.php
+++ resources/lang/en/article.php
@@ -2,4 +2,31 @@
 
 return [
     'index_title' => 'Articles',
+    'comment_form_title' => 'Add comment',
+    'comments_title' => 'Comments',
+    'edit_button' => 'Edit',
+    'destroy_button' => 'Destroy',
+    'my_article_title' => 'My Articles',
+    'add_article' => 'New Article',
+
+    'form' => [
+        'title_label' => 'Title',
+        'short_text_label' => 'Short text',
+        'text_label' => 'Article text',
+        'image_label' => 'Image',
+        'create_submit' => 'Create',
+    ],
+
+    'form_comment' => [
+        'comment_label' => 'Comment text',
+        'comment_submit' => 'Send',
+    ],
+
+    'table' => [
+        'id_title' => 'ID',
+        'title_title' => 'Title',
+        'created_at_title' => 'Created at',
+        'edit_title' => 'Edit',
+        'destroy_title' => 'Destroy',
+    ],
 ];
